5519abf6ee6fabbb2e0dc2018d3961c58b51ed1e
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-09 21:30:23 +0530

    code to take input number from user and checks even or odd

--- basics.java
+++ basics.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class basics {
+
     public static void main(String[] args) {
         System.out.println("Hello, World");
+
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        if(num % 2==0){
+            System.out.println("given number is even");
+        }else{
+            System.out.println("it is odd number");
+}
     }
 }
+
+
