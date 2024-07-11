d0d6c0a8005a0b2c4e2b190597356c62e707e23a
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-11 18:46:45 +0530

    recursive program to reverse the given string

---
+++ Demo.java
@@ -0,0 +1,53 @@
+// public class Demo
+//  {
+
+//     public int FibanacciNumber(int n){
+//         // int first=0;
+//         // int second=1;
+
+//         // if(n==0){
+//         //     return first;
+//         // }else if(n==1){
+//         //     return second;
+//         // }else{
+//         //     while(n>2){
+//         //         int next=first+second;
+//         //         first=second;
+//         //         second=next;
+//         //         n--;
+//         //     }
+//         //     return second;
+//         // }
+
+//         if(n<=1){
+//             return n;
+//         }else{
+//             return FibanacciNumber(n-1)+FibanacciNumber(n-2);
+//         }
+//     }
+    
+
+//     public static void main(String[] args) {
+//         Demo
+//          obj=new Demo
+//         ();
+//         int number=obj.FibanacciNumber(6);
+//         System.out.println(number);
+//     }
+    
+// }
+
+// public class Demo{
+//     public String reverseString(String name){
+//         if(name.isEmpty()){
+//             return name;
+//         }else{
+//             return reverseString(name.substring(1))+name.charAt(0);
+//         }
+//     }
+
+//     public static void main(String[] args) {
+//         Demo obj = new Demo();
+//         System.out.println(obj.reverseString("rakesh"));
+//     }
+// }
