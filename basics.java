1ad1df2aa60521a3489d0a80cb9f29bf47c7ecc6
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-11 10:22:33 +0530

    program with two constructors with deafult and one parameterized

--- basics.java
+++ basics.java
@@ -20,14 +20,45 @@ import java.util.Scanner;
 // }
 
 // Write a Java program to create a `String` object and print its length.
+// public class Basics {
+//     private void printStringLength(String str) {
+//         System.out.println(str.length());
+//     }
+
+//     public static void main(String[] args) {
+//         Basics obj = new Basics();
+//         obj.printStringLength("rakesh");
+//     }
+// }
+
+// Task: Write a Java program that creates an instance of a class with two constructors: one default and one parameterized, and then calls a method to display the values.
+
 public class Basics {
-    private void printStringLength(String str) {
-        System.out.println(str.length());
+    private String studentName;
+    private String collegeName;
+    public Basics() {
+        this.studentName = "XYZ";
+        this.collegeName = "ABC";
+    }
+
+    public Basics(String studentName, String collegeName) {
+        this.studentName = studentName;
+        this.collegeName = collegeName;
+    }
+
+    public void displayValues() {
+        System.out.println("Student Name: " + studentName);
+        System.out.println("College Name: " + collegeName);
     }
 
     public static void main(String[] args) {
-        Basics obj = new Basics();
-        obj.printStringLength("rakesh");
+        Basics obj1 = new Basics();
+        System.out.println("Using default constructor:");
+        obj1.displayValues();
+
+        Basics obj2 = new Basics("Rakesh", "IIITH Hyderabad");
+        System.out.println("Using parameterized constructor:");
+        obj2.displayValues();
     }
 }
 
