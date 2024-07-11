c02cf43df9cfe749ed8effd05863b409489fdbf9
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-11 16:32:51 +0530

    program to find average of positive numbers

--- Conditions.java
+++ Conditions.java
@@ -28,3 +28,34 @@
 
 // # Hard Challenge:
 // # - Task: Write a program that reads numbers until a negative number is entered and then prints the average of all positive numbers entered (exclude the negative one).
+// import java.util.Scanner;
+
+// public class Conditions {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+//         int sum = 0;
+//         int count = 0;
+
+//         while (true) {
+//             System.out.print("Enter a number: ");
+//             int number = scanner.nextInt();
+
+//             if (number < 0) {
+//                 break;
+//             }
+
+//             sum += number;
+//             count++;
+//         }
+
+//         if (count > 0) {
+//             double average = (double) sum / count;
+//             System.out.println("The average of the positive numbers entered is: " + average);
+//         } else {
+//             System.out.println("No positive numbers were entered.");
+//         }
+
+//         scanner.close();
+//     }
+// }
+
