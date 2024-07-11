d8e833fe6d2225b31e680f3301d12b22b22a3b84
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-11 19:38:10 +0530

    program to read integers using java,util.Scanner and return sum of integers

--- Libraries.java
+++ Libraries.java
@@ -14,11 +14,20 @@ import java.util.Random;
 
 // Medium Challenge:
 // - Task: Create a Java program that reads integers using `java.util.Scanner` until 0 is entered, then prints the sum of these integers.
-import 
+import java.util.Scanner;
 public class Libraries {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int sum=0;
         while(true){
-            
+            int num=scan.nextInt();
+            if(num==0){
+                System.out.println(num+sum);
+                break;
+            }else{
+                sum+=num;
+                System.out.println(sum);
+            }
         }
     }
 }
