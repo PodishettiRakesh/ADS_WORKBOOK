1dc2e82cd7c1035ca4b9589463b82649fe3deeaa
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-11 10:31:01 +0530

    created an array of  objects of a class and performed sorting based on one attribute

--- basics.java
+++ basics.java
@@ -33,33 +33,94 @@ import java.util.Scanner;
 
 // Task: Write a Java program that creates an instance of a class with two constructors: one default and one parameterized, and then calls a method to display the values.
 
+// public class Basics {
+//     private String studentName;
+//     private String collegeName;
+//     public Basics() {
+//         this.studentName = "XYZ";
+//         this.collegeName = "ABC";
+//     }
+
+//     public Basics(String studentName, String collegeName) {
+//         this.studentName = studentName;
+//         this.collegeName = collegeName;
+//     }
+
+//     public void displayValues() {
+//         System.out.println("Student Name: " + studentName);
+//         System.out.println("College Name: " + collegeName);
+//     }
+
+//     public static void main(String[] args) {
+//         Basics obj1 = new Basics();
+//         System.out.println("Using default constructor:");
+//         obj1.displayValues();
+
+//         Basics obj2 = new Basics("Rakesh", "IIITH Hyderabad");
+//         System.out.println("Using parameterized constructor:");
+//         obj2.displayValues();
+//     }
+// }
+
+
+
+// Task: Write a Java program to create an array of objects of a class containing multiple attributes,
+//  then perform sorting based on one attribute without using built-in sort functions.
 public class Basics {
     private String studentName;
     private String collegeName;
-    public Basics() {
-        this.studentName = "XYZ";
-        this.collegeName = "ABC";
-    }
+    private int studentID;
 
-    public Basics(String studentName, String collegeName) {
+    public Basics(String studentName, String collegeName, int studentID) {
         this.studentName = studentName;
         this.collegeName = collegeName;
+        this.studentID = studentID;
     }
 
-    public void displayValues() {
-        System.out.println("Student Name: " + studentName);
-        System.out.println("College Name: " + collegeName);
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getCollegeName() {
+        return collegeName;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void display() {
+        System.out.println("Student ID: " + studentID + ", Student Name: " + studentName + ", College Name: " + collegeName);
     }
 
     public static void main(String[] args) {
-        Basics obj1 = new Basics();
-        System.out.println("Using default constructor:");
-        obj1.displayValues();
+        Basics[] students = new Basics[5];
+        students[0] = new Basics("Rakesh", "IIITH Hyderabad", 3);
+        students[1] = new Basics("John", "MIT", 5);
+        students[2] = new Basics("Alice", "Stanford", 1);
+        students[3] = new Basics("Bob", "Harvard", 4);
+        students[4] = new Basics("Charlie", "Caltech", 2);
 
-        Basics obj2 = new Basics("Rakesh", "IIITH Hyderabad");
-        System.out.println("Using parameterized constructor:");
-        obj2.displayValues();
+        System.out.println("Before sorting:");
+        for (Basics student : students) {
+            student.display();
+        }
+
+        // Sorting based on studentID
+        for (int i = 0; i < students.length - 1; i++) {
+            for (int j = 0; j < students.length - 1 - i; j++) {
+                if (students[j].getStudentID() > students[j + 1].getStudentID()) {
+                    Basics temp = students[j];
+                    students[j] = students[j + 1];
+                    students[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("\nAfter sorting by studentID:");
+        for (Basics student : students) {
+            student.display();
+        }
     }
 }
-
 
