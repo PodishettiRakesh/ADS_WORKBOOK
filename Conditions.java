4c7bf67595ba504a6a028cd7f8f50bcee30bdb24
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-11 16:28:34 +0530

    program to check given number is even or odd

---
+++ Conditions.java
@@ -0,0 +1,21 @@
+
+// # - Task: Write a program that uses an `if-else` statement to check if a 
+// # number is positive or negative.
+// public class Conditions {
+//     public static void main(String[] args) {
+//         int number = 11;
+
+//         if (number >= 0) {
+//             System.out.println("The number is positive.");
+//         } else {
+//             System.out.println("The number is negative.");
+//         }
+//     }
+// }
+
+
+// # Medium programChallenge:
+// # - Task: Create a program that uses a `for` loop to print the first 10 multiples of a given number.
+
+// # Hard Challenge:
+// # - Task: Write a program that reads numbers until a negative number is entered and then prints the average of all positive numbers entered (exclude the negative one).
