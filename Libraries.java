b23fd241668234cab1a8cefdaa309d1a9ec103de
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-11 19:34:09 +0530

    program to generte the random number using Random package

---
+++ Libraries.java
@@ -0,0 +1,27 @@
+
+
+// Easy Challenge:
+// - Task: Write a simple Java program that uses the `java.util.Random` class to generate and print a random number.
+import java.util.Random;
+// public class Libraries {
+//     public static void main(String[] args) {
+//         Random ran=new Random();
+//         System.out.println(ran.nextInt());
+//     }
+// }
+
+
+
+// Medium Challenge:
+// - Task: Create a Java program that reads integers using `java.util.Scanner` until 0 is entered, then prints the sum of these integers.
+import 
+public class Libraries {
+    public static void main(String[] args) {
+        while(true){
+            
+        }
+    }
+}
+
+// Hard Challenge:
+// - Task: Implement a Java program that uses `java.io.BufferedReader` to read text from a file named "input.txt" and prints it to the console.
