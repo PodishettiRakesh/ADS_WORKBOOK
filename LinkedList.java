456072ece06c19293018e650ec2f08986878a1c1
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-12 12:30:46 +0530

    method to print the nodes in reverse order

--- LinkedList.java
+++ LinkedList.java
@@ -39,6 +39,20 @@ public class LinkedList {
         if (temp == null) return;
         prev.next = temp.next;
     }
+
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
     
 }
 class Node{
