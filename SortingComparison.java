683db235c51c8e60e4cf91eb46355c7ad3ae7873
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-18 11:40:37 +0530

    implemented radixsort to sort the given given positive integers

--- SortingComparison.java
+++ SortingComparison.java
@@ -53,3 +53,72 @@
 //         printArray(arr);
 //     }
 // }
+
+
+
+import java.util.Arrays;
+
+public class SortingComparison {
+
+    // Utility method to get maximum value in an array
+    public static int getMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
+
+    // Count Sort method used by Radix Sort
+    public static void countSort(int[] arr, int exp) {
+        int n = arr.length;
+        int[] output = new int[n];
+        int[] count = new int[10];
+        Arrays.fill(count, 0);
+
+        for (int i = 0; i < n; i++) {
+            count[(arr[i] / exp) % 10]++;
+        }
+
+        for (int i = 1; i < 10; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+            count[(arr[i] / exp) % 10]--;
+        }
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = output[i];
+        }
+    }
+
+    // Radix Sort method
+    public static void radixSort(int[] arr) {
+        int max = getMax(arr);
+        for (int exp = 1; max / exp > 0; exp *= 10) {
+            countSort(arr, exp);
+        }
+    }
+
+    // Utility method to print array
+    public static void printArray(int[] arr) {
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {170, 45, 75, 90, 802, 24, 2, 66};
+        System.out.println("Original array:");
+        printArray(arr);
+        radixSort(arr);
+        System.out.println("Sorted array using Radix Sort:");
+        printArray(arr);
+    }
+}
+i
