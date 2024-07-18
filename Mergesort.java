c358c4f0478574434c237c02804467db96d7cbc6
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-18 09:18:52 +0530

    merge sort implemented to sort the array of integers

--- Mergesort.java
+++ Mergesort.java
@@ -1,3 +1,69 @@
+// public class Mergesort {
+//     // Merge function for Merge Sort
+//     public static void merge(int[] arr, int l, int m, int r) {
+//         int n1 = m - l + 1;
+//         int n2 = r - m;
+
+//         int[] L = new int[n1];
+//         int[] R = new int[n2];
+
+//         for (int i = 0; i < n1; ++i) {
+//             L[i] = arr[l + i];
+//         }
+//         for (int j = 0; j < n2; ++j) {
+//             R[j] = arr[m + 1 + j];
+//         }
+
+//         int i = 0, j = 0;
+//         int k = l;
+//         while (i < n1 && j < n2) {
+//             if (L[i] <= R[j]) {
+//                 arr[k] = L[i];
+//                 i++;
+//             } else {
+//                 arr[k] = R[j];
+//                 j++;
+//             }
+//             k++;
+//         }
+
+//         while (i < n1) {
+//             arr[k] = L[i];
+//             i++;
+//             k++;
+//         }
+
+//         while (j < n2) {
+//             arr[k] = R[j];
+//             j++;
+//             k++;
+//         }
+//     }
+
+//     // Utility method to print array
+//     public static void printArray(int[] arr) {
+//         for (int i : arr) {
+//             System.out.print(i + " ");
+//         }
+//         System.out.println();
+//     }
+
+//     public static void main(String[] args) {
+//         int[] arr = {12, 11, 13, 5, 6, 7};
+//         int l = 0, m = arr.length / 2 - 1, r = arr.length - 1;
+        
+//         System.out.println("Original array:");
+//         printArray(arr);
+        
+//         merge(arr, l, m, r);
+        
+//         System.out.println("Array after merge:");
+//         printArray(arr);
+//     }
+// }
+
+
+
 public class Mergesort {
     // Merge function for Merge Sort
     public static void merge(int[] arr, int l, int m, int r) {
@@ -40,6 +106,18 @@ public class Mergesort {
         }
     }
 
+    // Merge Sort function
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+
     // Utility method to print array
     public static void printArray(int[] arr) {
         for (int i : arr) {
@@ -50,14 +128,13 @@ public class Mergesort {
 
     public static void main(String[] args) {
         int[] arr = {12, 11, 13, 5, 6, 7};
-        int l = 0, m = arr.length / 2 - 1, r = arr.length - 1;
         
         System.out.println("Original array:");
         printArray(arr);
         
-        merge(arr, l, m, r);
+        mergeSort(arr, 0, arr.length - 1);
         
-        System.out.println("Array after merge:");
+        System.out.println("Sorted array using Merge Sort:");
         printArray(arr);
     }
 }
