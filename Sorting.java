ddcec39a0fddcf649f8ab6c730d07cd8469db26f
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-18 09:12:04 +0530

    method to print the given array

--- Sorting.java
+++ Sorting.java
@@ -14,7 +14,6 @@ public class Sorting {
             arr[i] = temp;
         }
     }
-
     public static void printArray(int[] arr) {
         for (int i : arr) {
             System.out.print(i + " ");
