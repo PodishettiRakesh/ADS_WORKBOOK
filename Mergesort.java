c246a4cb42d1241b91df5a7552cba759722467ab
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-07-18 09:17:33 +0530

    mergesort program to sort the given array in java

---
+++ Mergesort.java
@@ -0,0 +1,64 @@
+public class Mergesort {
+    // Merge function for Merge Sort
+    public static void merge(int[] arr, int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for (int i = 0; i < n1; ++i) {
+            L[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; ++j) {
+            R[j] = arr[m + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+
+    // Utility method to print array
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        int l = 0, m = arr.length / 2 - 1, r = arr.length - 1;
+        
+        System.out.println("Original array:");
+        printArray(arr);
+        
+        merge(arr, l, m, r);
+        
+        System.out.println("Array after merge:");
+        printArray(arr);
+    }
+}
+
--- Sorting.java
+++ Sorting.java
@@ -70,73 +70,73 @@
 
 
 
-public class Sorting {
+// public class Sorting {
 
-    // Selection Sort Method
-    public static int selectionSort(int[] arr) {
-        int n = arr.length;
-        int swapCount = 0;
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
-                    minIndex = j;
-                }
-            }
-            if (minIndex != i) {
-                int temp = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = temp;
-                swapCount++;
-            }
-        }
-        return swapCount;
-    }
+//     // Selection Sort Method
+//     public static int selectionSort(int[] arr) {
+//         int n = arr.length;
+//         int swapCount = 0;
+//         for (int i = 0; i < n - 1; i++) {
+//             int minIndex = i;
+//             for (int j = i + 1; j < n; j++) {
+//                 if (arr[j] < arr[minIndex]) {
+//                     minIndex = j;
+//                 }
+//             }
+//             if (minIndex != i) {
+//                 int temp = arr[minIndex];
+//                 arr[minIndex] = arr[i];
+//                 arr[i] = temp;
+//                 swapCount++;
+//             }
+//         }
+//         return swapCount;
+//     }
 
-    // Insertion Sort Method
-    public static int insertionSort(int[] arr) {
-        int n = arr.length;
-        int swapCount = 0;
-        for (int i = 1; i < n; ++i) {
-            int key = arr[i];
-            int j = i - 1;
+//     // Insertion Sort Method
+//     public static int insertionSort(int[] arr) {
+//         int n = arr.length;
+//         int swapCount = 0;
+//         for (int i = 1; i < n; ++i) {
+//             int key = arr[i];
+//             int j = i - 1;
 
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                swapCount++;
-                j = j - 1;
-            }
-            arr[j + 1] = key;
-        }
-        return swapCount;
-    }
+//             while (j >= 0 && arr[j] > key) {
+//                 arr[j + 1] = arr[j];
+//                 swapCount++;
+//                 j = j - 1;
+//             }
+//             arr[j + 1] = key;
+//         }
+//         return swapCount;
+//     }
 
-    // Utility method to print array
-    public static void printArray(int[] arr) {
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-    }
+//     // Utility method to print array
+//     public static void printArray(int[] arr) {
+//         for (int i : arr) {
+//             System.out.print(i + " ");
+//         }
+//         System.out.println();
+//     }
 
-    public static void main(String[] args) {
-        int[] arr = {64, 25, 12, 22, 11};
+//     public static void main(String[] args) {
+//         int[] arr = {64, 25, 12, 22, 11};
 
-        int[] selectionSortArr = arr.clone();
-        int[] insertionSortArr = arr.clone();
+//         int[] selectionSortArr = arr.clone();
+//         int[] insertionSortArr = arr.clone();
 
-        System.out.println("Original array:");
-        printArray(arr);
+//         System.out.println("Original array:");
+//         printArray(arr);
 
-        int selectionSortSwaps = selectionSort(selectionSortArr);
-        int insertionSortSwaps = insertionSort(insertionSortArr);
+//         int selectionSortSwaps = selectionSort(selectionSortArr);
+//         int insertionSortSwaps = insertionSort(insertionSortArr);
 
-        System.out.println("Selection Sort Swaps: " + selectionSortSwaps);
-        System.out.println("Sorted array using Selection Sort:");
-        printArray(selectionSortArr);
+//         System.out.println("Selection Sort Swaps: " + selectionSortSwaps);
+//         System.out.println("Sorted array using Selection Sort:");
+//         printArray(selectionSortArr);
 
-        System.out.println("Insertion Sort Swaps: " + insertionSortSwaps);
-        System.out.println("Sorted array using Insertion Sort:");
-        printArray(insertionSortArr);
-    }
-}
+//         System.out.println("Insertion Sort Swaps: " + insertionSortSwaps);
+//         System.out.println("Sorted array using Insertion Sort:");
+//         printArray(insertionSortArr);
+//     }
+// }
